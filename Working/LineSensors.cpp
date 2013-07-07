55eb89b575c6a40035cefa67b8ab580bc91fcd3e
Toby-Scantlebury <toby.scantlebury@student.curtin.eduau>
2013-07-07 21:46:13 +0800

    Added some commenting to the LineSensor class files

--- Working/LineSensors.cpp
+++ Working/LineSensors.cpp
@@ -1,17 +1,22 @@
 #include "LineSensors.h"
 
+//Obtains a new reading and stores it in the public member reading[]
+//(Just calls readCalibrated(unsigned int*) using the public member reading[])
 inline void LineSensors::readCalibrated()
 {
 	PololuQTRSensors::readCalibrated(reading);
 }
 
+//Returns true if all values in public member reading[] are consistent with the specification in vals[]
+//Each item in vals may be either BLK, WHT or NUL.  That is black, white and null (disregard / don't care) respectively.
+//Thus, the function is used to check if the line sensor is reading the state specified in vals[].
 bool LineSensors::see(const LineSensor_ColourValues vals[QTR_NUM_PINS])
 {
 	for(int ii = 0; ii < QTR_NUM_PINS; ++ii)
 	{
 		if(
-		((vals[ii] == BLK) && (reading[ii] < MIN_BLACK)) ||
-		((vals[ii] == WHT) && (reading[ii] > MIN_BLACK))
+		((vals[ii] == BLK) && (reading[ii] < MIN_BLACK)) || //vals[ii] is black and reading[ii] isn't black OR
+		((vals[ii] == WHT) && (reading[ii] > MIN_BLACK))    //vals[ii] is white and reading[ii] isn't white
 		) { return false; }
 	}
 	return true;
--- Working/LineSensors.h
+++ Working/LineSensors.h
@@ -4,14 +4,25 @@
 #include "config.h"
 #include "PololuQTRSensors.h"
 
+//BLK = black, WHT = white, NUL = null/disregard/"don't care".
+//Used to specify the states of each QTR sensor in the array
 enum LineSensor_ColourValues {BLK, WHT, NUL};
 
 class LineSensors : public PololuQTRSensorsRC
 {
 	public:
 		LineSensors() : PololuQTRSensorsRC(QTR_SENSOR_PINS_LIST, QTR_NUM_PINS, QTR_TIMEOUT, QTR_EMITTER_PIN) {}
+
+		//Holds the last reading of the line sensors iff readCalibrated(void) is called
 		unsigned int reading[QTR_NUM_PINS];
+
+		//Obtains a new reading and stores it in the public member reading[]
+		//(Just calls readCalibrated(unsigned int*) using the public member reading[])
 		void readCalibrated();
+
+		//Returns true if all values in public member reading[] are consistent with the specification in vals[]
+		//Each item in vals may be either BLK, WHT or NUL.  That is black, white and null (disregard / don't care) respectively.
+		//Thus, the function is used to check if the line sensor is reading the state specified in vals[].
 		bool see(const LineSensor_ColourValues vals[8]);
 };
 
