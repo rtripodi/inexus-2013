eb4b47c2a1e9b0244389e152d7af67d13b2959c9
rtripod <rtripod@gmail.com>
2013-07-15 19:48:56 +0800

    Added more functions to GridMap class

--- Working/GridMap.cpp
+++ Working/GridMap.cpp
@@ -2,15 +2,49 @@
 
 GridMap::GridMap()
 {
-	//Create map data structure
+	for (unsigned char x = 0; x <= GRID_MAX_X; ++x)
+	{
+		for (unsigned char y = 0; x <= GRID_MAX_Y; ++y)
+		{
+			grid[x][y] = 0x00;
+		}
+	}
 }
 
+//Change desired flag(s) to 1 for passed point
 void GridMap::setFlag(Point *inPoint, unsigned char inFlag)
 {
 	(*inPoint).flags |= inFlag;
 }
 
+//Change desired flag(s) to 0 for passed point 
+void GridMap::removeFlag(Point *inPoint, unsigned char inFlag)
+{
+	(*inPoint).flags &= ~inFlag;
+}
+
+//Returns true if the passed flag(s) is/are set
 bool GridMap::isFlagSet(Point inPoint, unsigned char inFlag)
 {
-	return ((inPoint.flags & inFlag) ? true : false);
+	return (inPoint.flags & inFlag);
+}
+
+//Returns true if point is in grid
+bool GridMap::contains(Point point)
+{
+	return (point.x >= 0) && (point.x <= GRID_MAX_X) && (point.y >= 0) && (point.y <= GRID_MAX_Y);
+}
+
+//Returns true if there is a connection between pt1 and pt2, and are both inMaze()
+bool GridMap::pointsJoined(Point pt1, Point pt2)
+{
+	unsigned char xDel = abs(pt1.x - pt2.x);
+	unsigned char yDel = abs(pt1.y - pt2.y);
+	return contains(pt1) && contains(pt2) && ((xDel == 1 || yDel == 1) && (xDel != yDel));
+}
+
+//Returns true if point is not occupied
+bool GridMap::isPassable(Point point)
+{
+	return !(isFlagSet(point, OCCUPIED));
 }
--- Working/GridMap.h
+++ Working/GridMap.h
@@ -1,3 +1,12 @@
+/*
+	Grid directions:
+	       y          W
+	  .---->          ^
+	  |           S <   > N
+	  |               v 
+	x v               E
+*/
+
 #ifndef GridMap_h
 #define GridMap_h
 
@@ -10,21 +19,21 @@
 #include "Config.h"
 
 //Grid dimensions
-#define GRID_LENGTH (8)
-#define GRID_WIDTH (8)
+#define GRID_MAX_X (7)
+#define GRID_MAX_Y (7)
 
 //Known locations
-#define STARTING_X (0)
+#define STARTING_X (GRID_MAX_X)
 #define STARTING_Y (0)
 #define ENTRANCE_X (0)
-#define ENTRANCE_Y (GRID_LENGTH - 1)
-#define DROPZONE_X (GRID_WIDTH - 1)
-#define DROPZONE_Y (0)
+#define ENTRANCE_Y (0)
+#define DROPZONE_X (GRID_MAX_X)
+#define DROPZONE_Y (GRID_MAX_Y)
 
 struct Point
 {
-	int x;
-	int y;
+	unsigned char x;
+	unsigned char y;
 	unsigned char flags;
 };
 
@@ -50,17 +59,31 @@ struct Point
 #define GREEN		(0x02)
 #define BLUE		(0x01)
 
-class GridMap
+class GridMap//TODO: inherit Maze
 {
 	public:
 		GridMap();
 		
+		//Change desired flag(s) to 1 for passed point
 		void setFlag(Point *inPoint, unsigned char inFlag);
 		
+		//Change desired flag(s) to 0 for passed point 
+		void removeFlag(Point *inPoint, unsigned char inFlag);
+		
+		//Returns true if the passed flag(s) is/are set
 		bool isFlagSet(Point inPoint, unsigned char inFlag);
 		
+		//Returns true if point is in grid
+		bool contains(Point point);
+		
+		//Returns true if there is a connection between pt1 and pt2, and are both inMaze()
+		bool pointsJoined(Point pt1, Point pt2);
+		
+		//Returns true if point is not occupied
+		bool isPassable(Point point);
+		
 	private:
-		Point grid[GRID_LENGTH][GRID_WIDTH];	//	Placeholder, array index stored inside struct is not space efficent
+		unsigned char grid[GRID_MAX_X + 1][GRID_MAX_Y + 1];
 };
 
 #endif
