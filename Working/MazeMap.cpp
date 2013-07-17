6031a8837dcfc8bb842d78b7223c58cb070bc75a
Toby-Scantlebury <toby.scantlebury@student.curtin.eduau>
2013-07-17 16:06:45 +0800

    Maze Navigation Code by Luke

---
+++ Working/MazeMap.cpp
@@ -0,0 +1,3 @@
+#include "MazeMap.h"
+
+
---
+++ Working/MazeMap.h
@@ -0,0 +1,19 @@
+#ifndef MazeMap_h
+#define MazeMap_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+class MazeMap
+{
+	public:
+
+		
+	private:
+
+};
+
+#endif
