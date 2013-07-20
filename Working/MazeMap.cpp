2c32a449036f1393ab7fc8947f757d9ee32d7ff8
Ryan <rtripod@gmail.com>
2013-07-20 21:23:19 +0800

    Added alternate functions to MazeMap
    I have implemented functions to allow the turn record to be simplified
    as it is being created.
    I left most of Luke's code alone so he can complete it in the way he
    envisioned.  Some tweaks were made to keep it consistent with our coding
    standards.

--- Working/MazeMap.cpp
+++ Working/MazeMap.cpp
@@ -1,23 +1,24 @@
 #include "MazeMap.h"
 
-
 MazeMap::MazeMap()
 {
-    mazeLength=0;
-    index=0;
-    mazeDirection=1;
+    mazeLength = 0;
+    index = 0;
+    mazeDirection = 1;
+	needSimplify = false;
 }
 
 void MazeMap::updateMap(Direction inDirection)
 {
-    maze[index]=inDirection;
+    maze[index] = inDirection;
     index++;
 }
 
 MazeMap::Direction MazeMap::getNext()
 {
-    index+=mazeDirection;
-    return(maze[index-mazeDirection]);
+    Direction temp = maze[index];
+	index += mazeDirection;
+    return temp;
 }
         
 unsigned char MazeMap::simplify()
@@ -27,8 +28,61 @@ unsigned char MazeMap::simplify()
 void MazeMap::resetToStart()
 {
     mazeDirection = 1;
+	index = 0;
 }
 void MazeMap::resetToEnd()
 {
     mazeDirection = -1;
+	index = mazeLength;
+}
+
+void MazeMap::updateMapProgressively(Direction inDirection)
+{
+    if(needSimplify)
+	{
+		//TODO: I don't think the case of consecutive reversals is possible. Only put here as a safetynet for now
+		if (inDirection == reverse)
+		{
+			index -= 2;
+			mazeLength -= 2;
+			needSimplify = false;
+		}	
+		else
+			simplifyProgressively(inDirection);
+	}
+	else
+		maze[index] = inDirection;
+	if(maze[index] == reverse)
+		needSimplify = true;
+    index++;
+	mazeLength++;
+}
+
+void MazeMap::simplifyProgressively(Direction inDirection)
+{
+	index -= 2;
+	mazeLength -= 2;
+
+	//Left, Right, Forward, Back(reverse)
+	//L = RBF = FBR, R = LBF = FBL, F = LBL = RBR, B = LBR = RBL = FBF
+	if ( (maze[index] == right &&  inDirection == forward) || (maze[index] == forward &&  inDirection == right) )
+	{
+		maze[index] = left;
+		needSimplify = false;
+	}
+	else if ( (maze[index] == left &&  inDirection == forward) || (maze[index] == forward &&  inDirection == left) )
+	{
+		maze[index] = right;
+		needSimplify = false;
+	}
+	else if ( (maze[index] == left &&  inDirection == left) || (maze[index] == right &&  inDirection == right) )
+	{
+		maze[index] = forward;
+		needSimplify = false;
+	}
+	else if ( (maze[index] == left &&  inDirection == right) || (maze[index] == right &&  inDirection == left) || (maze[index] == forward &&  inDirection == forward) )
+	{
+		maze[index] = reverse;
+		needSimplify = true;
+	}
 }
--- Working/MazeMap.h
+++ Working/MazeMap.h
@@ -19,14 +19,24 @@ class MazeMap
         void resetToStart();
         void resetToEnd();
 
+		//Progressive implimentation:
+		void updateMapProgressively(Direction inDirection);
 		
 	private:
         Direction maze[MAZE_MAX];
         unsigned char mazeLength;
         unsigned char index;
-        unsigned char simplify();
         //forward is positive when going through the maze the first time, negative on it's way back to the start
         unsigned char mazeDirection;
+		
+        unsigned char simplify();
+		
+		//Progressive implimentation:
+		//Used to flag that a reverse has occured and must eventually be simplified
+		bool needSimplify;
+		
+		//Removes reverses from the maze map, simplifying 3 directions into 1
+		void simplifyProgressively(Direction inDirection);
 };
 
 #endif
