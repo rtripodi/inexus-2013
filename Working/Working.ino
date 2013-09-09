ae5d1a97ee9f2ab985938efaae0f237578b3185f
Ryan <rtripod@gmail.com>
2013-09-09 20:53:06 +0800

    Altered Working
    Fixed merging error.

--- Working/Working.ino
+++ Working/Working.ino
@@ -12,23 +12,33 @@
 #include "GridMap.h"
 #include "Routing.h"
 #include "GridNav.h"
+#include "MazeNav.h"
 #include "Colour.h"
 #include "ColourSoftware.h"
 
-ColourSoftware colour;
+Motor motors;
+LineSensors ls;
 
-MazeNav mazeNav(&motors, &mover, (IR**)&irs);
+Movement mover(&motors, &ls);
+
+IR irs[4] = {
+	IR(IR_SHORT_PIN, IR::shortRange),
+	IR(IR_MEDIUMR_PIN, IR::mediumRange),
+	IR(IR_MEDIUMB_PIN, IR::mediumRange),
+	IR(IR_MEDIUML_PIN, IR::mediumRange)
+};
+
+Claw claw(CLAW_LEFT_PIN, CLAW_RIGHT_PIN);
+
+GridNav gridNav(&motors, &mover, (IR**)&irs, &claw);
 
 void setup()
 {  
-	Serial.begin(9600);	
+ 	Serial.begin(9600);	
 	claw.setup();
 	motors.setup();
 	claw.shut();
 	gridNav.findBlock();
 }
 
-void loop()
-{
-	colour.senseColour();
-}
+void loop() {}
