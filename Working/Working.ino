34c3e0d73bdb2fec0f71df199b0108cb0dc9ab9b
rtripod <rtripod@gmail.com>
2013-09-16 15:08:23 +0800

    Moved GridNav into .ino file

--- Working/Working.ino
+++ Working/Working.ino
@@ -11,14 +11,41 @@
 #include "GridImports.h"
 #include "GridMap.h"
 #include "Routing.h"
-#include "GridNav.h"
-#include "MazeNav.h"
+#include "MazeMap.h"
 #include "Colour.h"
 #include "ColourSoftware.h"
+
+#define WALL_MIN (175)
+#define WALL_TOLERANCE (25)
 
 Motor motors;
 LineSensors ls;
 Movement mover(&motors, &ls);
+
+MazeMap mazeMap;
+
+typedef struct {
+	int frnt;
+	int rght;
+	int bck;
+	int lft;
+} IrValues;
+
+IrValues irInMm;
+
+IR frntIr = IR(IR_FRONT_PIN, IR::shortRange);
+IR rghtIr = IR(IR_RIGHT_PIN, IR::mediumRange);
+IR bckIr = IR(IR_BACK_PIN, IR::mediumRange);
+IR lftIr = IR(IR_LEFT_PIN, IR::mediumRange);
+/*
+IrSensors irs = {
+	&IR(IR_FRONT_PIN, IR::shortRange),
+	&IR(IR_RIGHT_PIN, IR::mediumRange),
+	&IR(IR_BACK_PIN, IR::mediumRange),
+	&IR(IR_LEFT_PIN, IR::mediumRange)
+};*/
+
+Claw claw(CLAW_LEFT_PIN, CLAW_RIGHT_PIN);
 
 void delayTillButton()
 {
@@ -33,37 +60,100 @@ void delayTillButton()
 	digitalWrite(LED_PIN, LOW);
 }
 
-/*IR frntIr = IR(IR_FRONT_PIN, IR::shortRange);
-IR rghtIr = IR(IR_RIGHT_PIN, IR::mediumRange);
-IR bckIr = IR(IR_BACK_PIN, IR::mediumRange);
-IR lftIr = IR(IR_LEFT_PIN, IR::mediumRange);*/
-
-IrSensors irs = {
-	&IR(IR_FRONT_PIN, IR::shortRange),
-	&IR(IR_RIGHT_PIN, IR::mediumRange),
-	&IR(IR_BACK_PIN, IR::mediumRange),
-	&IR(IR_LEFT_PIN, IR::mediumRange)
-};
-
-Claw claw(CLAW_LEFT_PIN, CLAW_RIGHT_PIN);
-
-GridNav gridNav(&motors, &mover, &irs, &claw);
-
 void setup()
 {  
 	Serial.begin(9600);	
 	claw.setup();
 	motors.setup();
 	claw.shut();
-	delayTillButton();
-	for (int ii = 0; ii < 300 ; ++ii)
-	{
-		ls.calibrate();
-	}
 }
 
 void loop()
 {
 	delayTillButton();
-	gridNav.findBlock();
+	firstNavigate();
+}
+
+//Navigate and map maze from starting point
+void firstNavigate()
+{
+	RelDir turn;
+	
+	searchInitEntrance();
+	while (1) //DEBUG: Need exit condition
+	{
+		searchNextMaze();
+		scanWalls();
+		turn = alwaysLeft();
+		mazeMap.updateMap(turn);
+	}
+}
+
+//Returns true if a wall is in given relative direction based on IR values
+bool isWall(RelDir relDir)
+{
+	if (relDir == FRONT)
+		return (irInMm.frnt < WALL_MIN + WALL_TOLERANCE);
+	else if (relDir == RIGHT)
+		return (irInMm.rght < WALL_MIN + WALL_TOLERANCE);
+	else if (relDir == BACK)
+		return (irInMm.bck < WALL_MIN + WALL_TOLERANCE);
+	else if (relDir == LEFT)
+		return (irInMm.lft < WALL_MIN + WALL_TOLERANCE);
+}
+
+//Updates the IR values
+void scanWalls()
+{
+	irInMm.frnt = frntIr.getDist();
+	irInMm.rght = rghtIr.getDist();
+	irInMm.bck = bckIr.getDist();
+	irInMm.lft = lftIr.getDist();
+}
+
+//From starting point, search for entrance
+void searchInitEntrance()
+{
+	scanWalls();
+	if (isWall(FRONT))
+	{
+		mover.rotateDirection(RIGHT, DEFAULT_SPEED);
+		while (isWall(LEFT))
+		{
+			mover.moveForward(DEFAULT_SPEED);
+			scanWalls();
+		}
+		mover.moveLength(170, DEFAULT_SPEED);
+		motors.stop();
+		mover.rotateDirection(LEFT, DEFAULT_SPEED);
+	}
+}
+
+void searchNextMaze()
+{
+	mover.moveLength(170, DEFAULT_SPEED);
+	motors.stop();
+	scanWalls();
+	while (!isWall(FRONT) && isWall(RIGHT) && isWall(LEFT))
+	{
+		mover.moveForward(DEFAULT_SPEED);
+	}
+	if (!isWall(FRONT))
+	{
+		mover.moveLength(170, DEFAULT_SPEED);
+		motors.stop();
+	}
+}
+
+//Separated to allow easier change to another turning scheme
+RelDir alwaysLeft()
+{
+	if (!isWall(LEFT))
+		return LEFT;
+	else if (!isWall(FRONT))
+		return FRONT;
+	else if (!isWall(RIGHT))
+		return RIGHT;
+	else
+		return BACK;
 }
